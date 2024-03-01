ea798349f32d5b8263b4aa073b1c1e6144f90767
HitmanUdhaya <udhaya.mohan1995@gmail.com>
2024-03-01 15:01:46 +0530

    Done

---
+++ src/ipl/IPL.java
@@ -0,0 +1,13 @@
+package ipl;
+
+public class IPL {
+public void c() {
+	System.out.println("rohit");
+}
+public void vc(){
+	System.out.println("bumrah");
+}
+public void coach() {
+	System.out.println("dravid");
+}
+}
